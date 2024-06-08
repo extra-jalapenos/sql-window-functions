cc86fe845c01d2ea29bea8eb74d302134e5e11b6
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-06-08 11:50:37 +0200

    correct table creation to only include 2015, not 2015 and onwards

--- standard_exercise/1_query.sql
+++ standard_exercise/1_query.sql
@@ -1,3 +1,3 @@
 CREATE TABLE `sql-sandbox-20240505.DataAnalytics.sf_bikeshare_2015` AS (
-  Select * from `bigquery-public-data.san_francisco.bikeshare_trips` WHERE EXTRACT(year from start_date) >= 2015
+  Select * from `bigquery-public-data.san_francisco.bikeshare_trips` WHERE EXTRACT(year from start_date) = 2015
 )
